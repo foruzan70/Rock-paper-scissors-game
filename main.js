455bfee6e8110db9269492fc6c25b71d12681ecb
foruzan70 <foruzan98@yahoo.com>
2025-02-13 23:34:36 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+const myChoise = document.getElementById("mychoise");
+const computerChoise = document.getElementById("computerChoise");
+const result = document.getElementById("result");
+let totalScore = document.getElementById("total-score");
+let score = {
+  bord:0,
+  bakht:0,
+  mosavi:0
+}
+
+function functionMain(userChoice) {
+  const computerNumber = Math.floor(Math.random() * 3 + 1);
+
+  if (userChoice === 'sang') {
+    if (computerNumber == 1) {
+      result.innerText = "مساوی";
+    } else if (computerNumber == 2) {
+      result.innerText = "باختی";
+    } else if (computerNumber == 3) {
+      result.innerText = "بردی";
+    }
+  
+    myChoise.src = "./mysrc/1.png";
+    computerChoise.src = `./mysrc/${computerNumber}.png`;
+    myChoise.style.display = "flex";
+    computerChoise.style.display = "flex";
+  }
+  
+  if (userChoice === 'Kaghaz') {
+    if (computerNumber == 1) {
+      result.innerText= "بردی";
+    } else if (computerNumber == 2) {
+      result.innerText= "مساوی";
+    } else if (computerNumber == 3) {
+      result.innerText= "باختی";
+    }
+  
+    myChoise.src="./mysrc/2.png";
+    computerChoise.src=`./mysrc/${computerNumber}.png`;
+    myChoise.style.display="flex";
+    computerChoise.style.display="flex";
+  }
+  
+  if (userChoice === 'Gheichi') {
+     if (computerNumber == 1) {
+       result.innerText= "باختی";
+     } else if (computerNumber == 2) {
+       result.innerText= "بردی";
+     } else if (computerNumber ==3 ) {
+       result.innerText= "مساوی";
+     }
+     
+     myChoise.src="./mysrc/3.png";
+     computerChoise.src=`./mysrc/${computerNumber}.png`;
+     myChoise.style.display="flex";
+     computerChoise.style.display="flex";
+  }
+  updateScore();
+
+}
+function updateScore(){
+if(result.innerText=== "بردی"){
+  score.bord++;
+  }else if(result.innerText=== "باختی"){
+    score.bakht++;
+  }else if(result.innerText=== "مساوی"){
+    score.mosavi++;
+  }
+  console.log(totalScore);
+  totalScore.innerText = `مساوی:${score.mosavi}، برد:${score.bord}، باخت:${score.bakht}`;
+}
+
